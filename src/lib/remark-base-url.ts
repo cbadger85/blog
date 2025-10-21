842030f63c14f04c782339fc0423b827a168d862
Charles Badger <charles.badger@gmail.com>
2025-10-21 17:02:03 -0500

    update base url for images

--- src/lib/remark-base-url.ts
+++ src/lib/remark-base-url.ts
@@ -1,13 +1,14 @@
 import type { RemarkPlugin } from "@astrojs/markdown-remark";
 import { visit } from "unist-util-visit";
 
-const base = process.env.NODE_ENV === "production" ? "/blog" : "/";
+// This needs to be hard-coded until I can figure out how to use the pipeline variable.
+const base = process.env.NODE_ENV === "production" ? "/blog" : "";
 
 export const remarkBaseUrl: RemarkPlugin = () => {
   return (tree) => {
     visit(tree, "image", (node) => {
       if (node.url && node.url.startsWith("/") && !node.url.startsWith("//")) {
-        node.url = base + node.url.slice(1);
+        node.url = base + node.url;
       }
     });
   };
