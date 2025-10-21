8762438ba554e595f9ffcd9627acb2e299f64a04
Charles Badger <charles.badger@gmail.com>
2025-10-21 16:55:07 -0500

    test base url in ci/cd

--- src/lib/remark-base-url.ts
+++ src/lib/remark-base-url.ts
@@ -7,6 +7,8 @@ export const remarkBaseUrl: RemarkPlugin = () => {
   return (tree) => {
     visit(tree, "image", (node) => {
       // Only modify absolute paths that start with /
+      console.log({ base });
+
       if (node.url && node.url.startsWith("/") && !node.url.startsWith("//")) {
         node.url = base + node.url.slice(1);
       }
