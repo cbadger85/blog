56842f7243eb37ee8d04b82601a51f298c432e73
Charles Badger <charles.badger@gmail.com>
2025-10-21 16:58:31 -0500

    test base url in ci/cd

--- src/lib/remark-base-url.ts
+++ src/lib/remark-base-url.ts
@@ -1,14 +1,11 @@
 import type { RemarkPlugin } from "@astrojs/markdown-remark";
 import { visit } from "unist-util-visit";
 
-const base = import.meta.env.BASE_URL;
+const base = process.env.NODE_ENV === "production" ? "/blog" : "/";
 
 export const remarkBaseUrl: RemarkPlugin = () => {
   return (tree) => {
     visit(tree, "image", (node) => {
-      // Only modify absolute paths that start with /
-      console.log({ base });
-
       if (node.url && node.url.startsWith("/") && !node.url.startsWith("//")) {
         node.url = base + node.url.slice(1);
       }
